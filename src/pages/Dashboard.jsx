89b8dbf19ae6ecd13b5d9d1602e110f03500fbb4
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2025-05-06 15:05:02 +0300

    Side-Bar
    Made the SideBar for the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,17 @@
 import "../styles/dashboard.css";
 import TopBar from "../context/TopBar";
+import SideBar from "../context/SideBar";
 
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <TopBar />
-      <div className="dashboard-content">
-        <h1>Welcome to the Dashboard!</h1>
+      <div className="dashboard-body">
+        <SideBar />
+        <div className="dashboard-content">
+          <h1>Welcome to the Dashboard!</h1>
+        </div>
       </div>
     </div>
   );
